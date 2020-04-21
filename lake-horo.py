28bdc3fd8edc646bc4bf59f0b971781bc00f173a
koushik <kshanka2@c53.localdomain>
2020-04-21 13:22:12 -0400

    asd

--- lake-horo.py
+++ lake-horo.py
@@ -115,43 +115,51 @@ damping = tf.placeholder(tf.float32, shape=())
 U  = tf.Variable(u_init)
 Ut = tf.Variable(ut_init)
 
-U_full = tf.Variable(np.zeros([(3*N)/2, N], dtype=np.float32))
-Ut_full = tf.Variable(np.zeros([(3*N)/2, N], dtype=np.float32))
+
+#The complete 3N/2 * N matrices on which we'll perform calculations
+#U_full = tf.Variable(np.zeros([(3*N)/2, N], dtype=np.float32))          
+#Ut_full = tf.Variable(np.zeros([(3*N)/2, N], dtype=np.float32))
+U_send = tf.Variable(np.zeros([(N)/2, N], dtype=np.float32))          
+Ut_send = tf.Variable(np.zeros([(N)/2, N], dtype=np.float32))          
+
 
 
 rank_bcast = tf.group(
-  tf.assign(U_full[N:], hvd.broadcast(U[:N/2], 1)),  
-  tf.assign(Ut_full[N:], hvd.broadcast(Ut[:N/2], 1)),  
-  tf.assign(U_full[:N/2], hvd.broadcast(U[N/2:], 0)),  
-  tf.assign(Ut_full[:N/2], hvd.broadcast(Ut[N/2:], 0)))  
+  tf.assign(U[:N/2], hvd.broadcast(U_send, 1)),  #Sending 1st half of rank 1 to rank 0 for U
+  tf.assign(Ut[:N/2], hvd.broadcast(Ut_send, 1)),  #Sending 1st half of rank 1 to rank 0 for Ut
+  tf.assign(U[N/2:], hvd.broadcast(U_send, 0)),  #Sending 2nd half of rank 0 to rank 1 for U
+  tf.assign(Ut[N/2:], hvd.broadcast(Ut_send, 0)))  #Sending 2nd half of rank 0 to rank 1 for Ut
 
-U_full_rank0_group = tf.group(                         
-    U_full[:N].assign(U),
-    Ut_full[:N].assign(Ut))
+#Copy the rest of U and Ut for rank 0
+U_rank0_group = tf.group(                         
+    U_send.assign(U[N/2:]),
+    Ut_send.assign(Ut[N/2:]))
 
-U_full_rank1_group = tf.group(
-      U_full[N/2:].assign(U),
-      Ut_full[N/2:].assign(Ut))
+#Copy the rest of U and Ut for rank 1
+U_rank1_group = tf.group(
+      U_send.assign(U[:N/2]),
+      Ut_send[N/2:].assign(Ut[:N/2]))
 
 # Discretized PDE update rules
-U_ = U_full + eps * Ut_full
-Ut_ = Ut_full + eps * (laplace(U_full) - damping * Ut_full)
+U_ = U + eps * Ut
+Ut_ = Ut + eps * (laplace(U) - damping * Ut)
 
 
 # Operation to update the state
 step = tf.group(
-  U_full.assign(U_),
-  Ut_full.assign(Ut_))
+  U.assign(U_),
+  Ut.assign(Ut_))
 
 
-#Operation to extract back the actual size
-rank0_extract = tf.group(
-  U.assign(tf.slice(U_full,[0,0],[N,N])),
-  Ut.assign(tf.slice(Ut_full,[0,0],[N,N])))
+#Get the matrix U and Ut from U_full and Ut_full for rank 0
+#rank0_extract = tf.group(
+  #U.assign(tf.slice(U_full,[0,0],[N,N])),
+  #Ut.assign(tf.slice(Ut_full,[0,0],[N,N])))
 
-rank1_extract = tf.group(
-  U.assign(tf.slice(U_full,[N/2,0],[N,N])),
-  Ut.assign(tf.slice(Ut_full,[N/2,0],[N,N])))
+#Get the matrix U and Ut from U_full and Ut_full for rank 1
+#rank1_extract = tf.group(
+  #U.assign(tf.slice(U_full,[N/2,0],[N,N])),
+  #Ut.assign(tf.slice(Ut_full,[N/2,0],[N,N])))
 
 # Initialize state to initial conditions
 tf.global_variables_initializer().run()
@@ -160,18 +168,14 @@ tf.global_variables_initializer().run()
 start = time.time()
 for i in range(num_iter):
   # Step simulation
-  rank_bcast.run()         
-  if hvd.rank() == 0:       
-      U_full_rank0_group.run()    
+  if hvd.rank() == 0:      # Fill out the rest of the values for U_full and Ut_full
+      U_rank0_group.run()    
   else:
-      U_full_rank1_group.run()
+      U_rank1_group.run()
+
+  rank_bcast.run()      #Send the upper and lower values for rank 1 and 0 respectively for matrix U and Ut
 
   step.run({eps: 0.06, damping: 0.03})    
-
-  if hvd.rank() == 0:           
-      rank0_extract.run()
-  else:
-      rank1_extract.run()
 
 end = time.time()
 
